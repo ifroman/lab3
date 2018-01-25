a717d2fc56397a693f8fbd9a1b023febe85ed859
Isabella Froman <ifroman@ucsd.edu>
2018-01-25 10:51:47 -0800

    changes

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,30 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Change text test");
+		$("#testjs").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
 }
+
+function projectClick(e) { 
+    e.preventDefault();
+    console.log("hi mom");
+
+    $(this).css("background-color", "#7fff00");
+   var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(".project-description").fadeIn();
+       $(".project-description").fadeOut();
+
+    }
+}
+
+
